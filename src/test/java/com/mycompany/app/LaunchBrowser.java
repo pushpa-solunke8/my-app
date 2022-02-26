743293915729ec2608a202e5ece4ff832fa17bc0
Ganesh Solunke <ganesh_solunke@persistent.com>
2022-02-26 12:25:09 +0530

    added firefox browser

--- src/test/java/com/mycompany/app/LaunchBrowser.java
+++ src/test/java/com/mycompany/app/LaunchBrowser.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 
@@ -15,6 +16,11 @@ public class LaunchBrowser {
 		System.setProperty("webdriver.chrome.driver", "D:\\newversion98\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://kite.zerodha.com");
+	}
+	public void openbrowser() {
+		System.setProperty("webdriver.chrome.driver", "D:\\geckfirefox\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://facebook.com");
 	}
 
 }
